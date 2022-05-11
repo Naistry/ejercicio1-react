5d55d70a955de8c2fbb478873cb1b7121fae0b52
Martin Pastorino <martinpastorino013@gmail.com>
2022-05-11 01:35:35 -0300

    Coreccion de nombre

--- src/components/componentA.jsx
+++ src/components/componentA.jsx
@@ -5,7 +5,7 @@ import { Contacto } from '../models/contacto.class'
 const ComponentA = ({contacto}) => {
   return (
     <div>
-        <h1>Nombre: {Contacto.nombre} </h1>
+        <h1>Nombre: {contacto.nombre} </h1>
         <h1>Apellido: {contacto.apellido}</h1>
         <h1>E-mail: {contacto.email}</h1>
         <h1>Conectado: {contacto.conectado ?'Está conectado':'No conectado'}</h1>
